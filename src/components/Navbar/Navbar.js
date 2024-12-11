e103818ba9410c43d7f79e785eee25ba62a00f7e
DjangoCodeLab <sksg203@gmail.com>
2024-12-11 12:54:39 +0530

    some changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,42 +5,19 @@ import MovieSearch from '../MovieSearch/MovieSearch';
 export default function Navbar() {
     return (
         <>
-           <header className="d-flex flex-wrap justify-content-center py-1 mb-4 border border-dark rounded">
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <span className="fs-4 mx-5">MovieDb</span>
-    </a>
-    <ul className="nav nav-pills mx-2 px-5">
-        <li className="nav-item header__list">
-            <Link 
-                to="/popular" 
-                className="nav-link text-secondary hover:text-white"
-                activeClassName="text-white"
-            >
-                Popular
-            </Link>
-        </li>
-        <li className="nav-item header__list">
-            <Link 
-                to="/top_rated" 
-                className="nav-link text-secondary hover:text-white"
-                activeClassName="text-white"
-            >
-                Top Rated
-            </Link>
-        </li>
-        <li className="nav-item header__list">
-            <Link 
-                to="/upcoming" 
-                className="nav-link text-secondary hover:text-white"
-                activeClassName="text-white"
-            >
-                Upcoming
-            </Link>
-        </li>
+            <header className="d-flex flex-wrap justify-content-center py-1 mb-4 border border-dark rounded">
+                <a href="/" className="  d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <span className="fs-4 mx-5">MovieDb</span>
+                </a>
+                <ul className="nav nav-pills mx-2 px-5">
+                    <Link to="/popular" className="nav-item header__list"><li className=" nav-link text-secondary nav-itme">Popular</li></Link>
+                    <Link to="/top_rated" className="nav-item header__list"><li className=" nav-link text-secondary">Top Rated</li></Link>
+                    <Link to="/upcoming" className="nav-item header__list"><li className=" nav-link text-secondary">Upcoming</li></Link>
 
-        <MovieSearch />
-    </ul>
-</header>
+                    <MovieSearch />
+                 
+                </ul>
+            </header>
 
         </>
     )
